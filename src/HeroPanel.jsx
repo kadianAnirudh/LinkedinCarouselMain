e961385a0551b82992c9a92f93c9cb41456a10f3
unknown <coderanirudh@gmail.com>
2023-03-10 14:37:58 +0530

    march 10 commit 2

--- src/HeroPanel.jsx
+++ src/HeroPanel.jsx
@@ -147,12 +147,14 @@ const HeroPanel = () => {
                 engagement magnets.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
-                <a
-                  href="/sam"
-                  className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Get started
-                </a>
+                <Link to="sam">
+                  <a
+                    // href="/sam"
+                    className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Get started
+                  </a>
+                </Link>
                 {/* <a href="https://calendly.com/anirudh2024kadian/30min?month=2023-02&date=2023-02-18" className="text-base font-semibold leading-7 text-gray-900">
                   Lets Talk <span aria-hidden="true">→</span>
                 </a> */}
