271810e72adba2b124c93ced689944bc179d4ef8
unknown <coderanirudh@gmail.com>
2023-03-16 20:59:31 +0530

    added Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Sam from "./Creators/Sam/Sam.jsx";
+import ReactGA from "react-ga";
 
 function App() {
+  function initializeReactGA() {
+    ReactGA.initialize("G-FRTXDTS3MK");
+    ReactGA.pageview("/");
+  }
+
+  useEffect(() => {
+    initializeReactGA();
+  }, []);
+
   return (
     <div className="App">
       <Routes>
